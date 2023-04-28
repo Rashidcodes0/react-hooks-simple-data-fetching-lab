825916c1ad0909eb5c5be5b114a0742a41c255a2
Abdirashidnoor <abdirashid16112@gmail.com>
2023-04-28 14:50:19 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [loading, setLoading] = useState(true);
+    const [imageUrl, setImageUrl] = useState("");
+
+    useEffect(() => {
+        async function fetchRandomDogImage() {
+            setLoading(true);
+            const response = await fetch(
+                "https://dog.ceo/api/breeds/image/random"
+            );
+            const data = await response.json();
+            setImageUrl(data.message);
+            setLoading(false);
+        }
+
+        fetchRandomDogImage();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <img src={imageUrl} alt="A Random Dog" />
+        </div>
+    );
+}
+
+export default App;
